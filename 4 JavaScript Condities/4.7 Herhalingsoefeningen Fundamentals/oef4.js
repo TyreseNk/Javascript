ff599ba14b14ab5f6f51c4fb45dfe1ee8438e671
TyreseNkanta <tyrese.nkanta@icloud.com>
2024-10-07 14:35:38 +0200

    oefening 4

--- 4 JavaScript Condities/4.7 Herhalingsoefeningen Fundamentals/oef4.js
+++ 4 JavaScript Condities/4.7 Herhalingsoefeningen Fundamentals/oef4.js
@@ -3,15 +3,13 @@ const grootte = Number(prompt("geef aantal lijnen in. "));
 let regel ="";
 for (let i= 0 ; i< grootte;i++) {
     if(i%2==0){
-
-    
-    for(let j= 1 ; j<= grootte;j++){
-        if(j % 2 !=0){
-            regel+="#";
-        }else{
-            regel+=" ";
+        for(let j= 1 ; j<= grootte;j++){
+            if(j % 2 !=0){
+                regel+="#";
+            }else{
+                regel+=" ";
+            }
         }
-    }
     }else{
         for(let j= 1 ; j<= grootte;j++){
             if(j % 2 ==0){
